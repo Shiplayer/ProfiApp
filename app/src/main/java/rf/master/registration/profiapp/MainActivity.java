ddb693310d7db2487f8bf8a344d33f769aa86a89
Shiplayer <shiplayer@yandex.ru>
2018-03-29 01:13:05 +0300

    add adapter for recycler view, update layout for card view

--- app/src/main/java/rf/master/registration/profiapp/MainActivity.java
+++ app/src/main/java/rf/master/registration/profiapp/MainActivity.java
@@ -3,16 +3,28 @@ package rf.master.registration.profiapp;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
+import rf.master.registration.profiapp.data.entity.Store;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getCanonicalName();
     private TextView mTextMessage;
     private RecyclerView mMainList;
+    private static String[] RANDOM_FIRST_NAME = {"Kaitlin", "Garnett", "Moe", "Tiger", "Fletcher"};
+    private static String[] RANDOM_LAST_NAME = {"Marcia", "Cecil", "Phyliss", "Cass", "Michaela"};
+    private static String[] RANDOM_FAKE_NAME_STORIES = {"The Devil is a Girl", "The Chosen Ones sister", "List of Random Names"};
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -21,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_search:
+                    Snackbar.make(item.getActionView(), R.string.title_home, 100).show();
                     mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_favorites:
@@ -44,9 +57,26 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         mMainList = findViewById(R.id.rv_cards);
         mMainList.setLayoutManager(new LinearLayoutManager(this));
+        mMainList.setAdapter(new StoresAdapter(getRandomListStories(20)));
+        mMainList.setHasFixedSize(true);
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private List<Store> getRandomListStories(int count){
+        List<Store> list = new ArrayList<>();
+        Random random = new Random();
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i = 0; i < count; i++){
+            list.add(new Store(getResources(), RANDOM_FAKE_NAME_STORIES[random.nextInt(RANDOM_FAKE_NAME_STORIES.length)],
+                    RANDOM_FIRST_NAME[random.nextInt(RANDOM_FIRST_NAME.length)], RANDOM_LAST_NAME[random.nextInt(RANDOM_LAST_NAME.length)],
+                    random.nextFloat() * 10 % 5, random.nextFloat() * 10 % 5));
+            stringBuilder.append(list.get(i)).append("\n");
+        }
+
+        Log.w(TAG, "stories = " + stringBuilder.toString());
+        return list;
     }
 
 }
---
+++ app/src/main/java/rf/master/registration/profiapp/StoresAdapter.java
@@ -0,0 +1,72 @@
+package rf.master.registration.profiapp;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import rf.master.registration.profiapp.data.entity.Store;
+
+/**
+ * Created by Shiplayer on 28.03.18.
+ */
+
+public class StoresAdapter extends RecyclerView.Adapter<StoresAdapter.ViewHolder> {
+    private List<Store> mListStories;
+
+    public StoresAdapter(List<Store> stories){
+        mListStories = stories;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        public CardView cardView;
+        public ImageView storePhoto;
+        public TextView firstName;
+        public TextView lastName;
+        public RatingBar storiesRating;
+        public RatingBar costRating;
+        public TextView tvCostShow;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            cardView = itemView.findViewById(R.id.cv_stories_list);
+            storePhoto = itemView.findViewById(R.id.iv_store_photo);
+            firstName = itemView.findViewById(R.id.tv_firstName);
+            lastName = itemView.findViewById(R.id.tv_lastName);
+            storiesRating = itemView.findViewById(R.id.rb_stories);
+            costRating = itemView.findViewById(R.id.rb_costs);
+            tvCostShow = itemView.findViewById(R.id.tv_cost_show);
+        }
+    }
+
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view_contain, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.storePhoto.setImageDrawable(mListStories.get(position).getTitleImage());
+        holder.firstName.setText(mListStories.get(position).getFirstNameManager());
+        holder.lastName.setText(mListStories.get(position).getLastNameManager());
+        holder.storiesRating.setRating(mListStories.get(position).getShopRating());
+        holder.costRating.setRating(mListStories.get(position).getCostRating());
+        holder.tvCostShow.setText(String.valueOf((float) Math.round(mListStories.get(position).getCostRating()*10)/10));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mListStories.size();
+    }
+
+    public void changeStories(List<Store> stories){
+        this.mListStories = stories;
+        notifyDataSetChanged();
+    }
+}
