2f595def7b8aaf82bfb80018e53a3b1237907f09
Shiplayer <shiplayer@yandex.ru>
2018-04-09 01:24:31 +0300

    implements parcelable for entity

--- app/src/main/java/rf/master/registration/profiapp/data/entity/Store.java
+++ app/src/main/java/rf/master/registration/profiapp/data/entity/Store.java
@@ -2,6 +2,8 @@ package rf.master.registration.profiapp.data.entity;
 
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +14,7 @@ import rf.master.registration.profiapp.R;
  * Created by Shiplayer on 28.03.18.
  */
 
-public class Store {
+public class Store implements Parcelable{
     private static Drawable titleImage;
     private String nameStore;
     private String firstNameManager;
@@ -24,6 +26,28 @@ public class Store {
     public Store(){
 
     }
+
+    public Store(Parcel in){
+
+        nameStore = in.readString();
+        firstNameManager = in.readString();
+        lastNameManager = in.readString();
+        shopRating = in.readFloat();
+        costRating = in.readFloat();
+        employeeList = in.createTypedArrayList(Employee.CREATOR);
+    }
+
+    public static final Creator<Store> CREATOR = new Creator<Store>() {
+        @Override
+        public Store createFromParcel(Parcel in) {
+            return new Store(in);
+        }
+
+        @Override
+        public Store[] newArray(int size) {
+            return new Store[size];
+        }
+    };
 
     public Store(Resources resources, String nameStore, String firstNameManager, String lastNameManager, float shopRating, float costRating) {
         if(titleImage == null)
@@ -101,5 +125,29 @@ public class Store {
                 ", shopRating=" + shopRating +
                 ", costRating=" + costRating +
                 '}';
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    /*
+    private String nameStore;
+    private String firstNameManager;
+    private String lastNameManager;
+    private float shopRating;
+    private float costRating;
+    private List<Employee> employeeList;
+     */
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(nameStore);
+        dest.writeString(firstNameManager);
+        dest.writeString(lastNameManager);
+        dest.writeFloat(shopRating);
+        dest.writeFloat(costRating);
+        dest.writeList(employeeList);
     }
 }
